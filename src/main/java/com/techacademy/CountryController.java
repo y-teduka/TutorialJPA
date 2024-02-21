d37243361769de8f25ebabf332b3d76954297647
y-teduka <dc733243@ra2.so-net.ne.jp>
2024-02-21 21:23:51 +0900

    Lesson 16Chapter 6ここまでで作成したコードのコミット

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -3,7 +3,10 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("country")
@@ -21,5 +24,31 @@ public class CountryController {
         model.addAttribute("countrylist", service.getCountryList());
         // country/list.htmlに画面遷移
         return "country/list";
+    }
+
+//-------詳細画面-------
+    @GetMapping(value = { "/detail", "/detail/{code}/" })
+    public String getCountry(@PathVariable(name = "code", required = false) String code, Model model) {
+        Country country = code != null ? service.getCountry(code) : new Country();
+        model.addAttribute("country", country);
+        return "country/detail";
+    }
+
+    @PostMapping("/detail")
+    public String postCountry(@RequestParam("code") String code, @RequestParam("name") String name,
+            @RequestParam("population") int population, Model model) {
+        service.updateCountry(code, name, population);
+        return "redirect:/country/list";
+    }
+
+    @GetMapping("/delete")
+    public String deleteCountryForm(Model model) {
+        return "country/delete";
+    }
+
+    @PostMapping("/delete")
+    public String deleteCountry(@RequestParam("code") String code, Model model) {
+        service.deleteCountry(code);
+        return "redirect:/country/list";
     }
 }
--- src/main/java/com/techacademy/CountryService.java
+++ src/main/java/com/techacademy/CountryService.java
@@ -1,9 +1,12 @@
 package com.techacademy;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class CountryService {
@@ -18,5 +21,23 @@ public class CountryService {
     public List<Country> getCountryList() {
         // リポジトリのfindAllメソッドを呼び出す
         return repository.findAll();
+    }
+
+    public Country getCountry(String code) {
+        Optional<Country> option = repository.findById(code);
+        Country country = option.orElse(null);
+        return country;
+    }
+
+    @Transactional
+    public void updateCountry(String code, String name, int population) {
+        Country country = new Country(code, name, population);
+        repository.save(country);
+    }
+
+    @Transactional
+    public void deleteCountry(String code) {
+        repository.deleteById(code);
+
     }
 }
